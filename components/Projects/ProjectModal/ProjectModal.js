540dcea721a6ba50160a6a32007773247d2b5a2e
Geremi Ramos <geremiramosm@gmail.com>
2021-12-20 03:41:09 -0400

    Done with project modal and skills section

--- components/Projects/ProjectModal/ProjectModal.js
+++ components/Projects/ProjectModal/ProjectModal.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Modal,
   ModalBody,
   ModalOverlay,
@@ -7,39 +8,72 @@ import {
   ModalFooter,
   ModalHeader,
   ModalCloseButton,
+  Flex,
+  Text,
+  Tooltip,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { ProjectImages } from "./ProjectImages";
 
-function importAll(r) {
-  let images = [];
-  r.keys().map((item) => {
-    images = [...images, item.replace("./", "")];
-  });
-  return images;
-}
+import { PROJECTS_DATA, techIcons } from "../config";
 
-const images = importAll(
-  require.context(
-    "../../../public/images/projects/Countries-Api",
-    false,
-    /\.(png|jpe?g|svg)$/
-  )
-);
+export const ProjectModal = ({ id, isOpen, onClose, project }) => {
+  const [data, setData] = useState(null);
+  const [images, setImages] = useState(null);
 
-export const ProjectModal = ({ id, isOpen, onClose }) => {
+  useEffect(() => {
+    if (project) {
+      setData(PROJECTS_DATA[project.name]);
+      setImages(PROJECTS_DATA[project.name].images);
+    }
+  }, [project]);
+
+  if (!data || !images) return <></>;
+
+  const renderTechIcons = () => {
+    return data.stack.map((tech) => (
+      <Tooltip label={tech.toUpperCase()}>
+        <Text mx="0.5rem">{techIcons[tech]}</Text>
+      </Tooltip>
+    ));
+  };
+
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent maxW="200px">
-          <ModalHeader>{id}</ModalHeader>
+        <ModalContent
+          bg="rgba(25,25,25,0.75)"
+          backdropFilter="blur(20px)"
+          color="white"
+        >
+          <ModalHeader
+            fontSize="1.75rem"
+            textTransform="uppercase"
+            textAlign="center"
+          >
+            {data.title}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>{id}</ModalBody>
+          <ModalBody>
+            <ProjectImages project={data.title} slides={images} />
+            <Box mt="2rem">
+              <Text lineHeight="1.75rem">{data.desc}</Text>
+            </Box>
+          </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
+          <ModalFooter display="block">
+            <Box
+              textAlign="center"
+              mb="1.25rem"
+              textTransform="uppercase"
+              fontWeight="700"
+            >
+              <Text>Tech Stack: </Text>
+            </Box>
+            <Flex justify="center" fontSize="1.75rem" w="100%">
+              {renderTechIcons()}
+            </Flex>
           </ModalFooter>
         </ModalContent>
       </Modal>
